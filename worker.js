5c783b29da5a4176527cc47b02c2145c31a88b4b
lasokar <129457995+lasokar@users.noreply.github.com>
2026-04-06 10:12:22 -0700

    hm

--- worker.js
+++ worker.js
@@ -10,17 +10,37 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  console.log('request:', url.pathname);
-  if (url.pathname.includes('1.txt')) {
-    event.respondWith(
-      fetch(`/geometrydashdotcom/assets/levels/${levelID}.txt`)
-    );
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);  
+  if (levelID < 0) {
+    if (url.pathname.includes("1.txt")) {
+      event.respondWith(
+        fetch(`/geometrydashdotcom/assets/levels/${levelID}.txt`)
+      );
+      return;
+    }
+
+    if (url.pathname.includes("StereoMadness.mp3")) {
+      event.respondWith(
+        fetch(`/geometrydashdotcom/assets/music/${levelID}.mp3`)
+      );
+      return;
+    }
   }
-   if (url.pathname.includes('StereoMadness.mp3')) {
-    event.respondWith(
-      fetch(`/geometrydashdotcom/assets/music/${levelID}.mp3`)
-    );
+  
+  if (levelID >= 0) {
+    if (url.pathname.includes("1.txt")) {
+      event.respondWith(
+        fetch(`https://getleveldata.lasokar.workers.dev?id=${levelID}`)
+      );
+      return;
+    }
+
+    if (url.pathname.includes("StereoMadness.mp3")) {
+      event.respondWith(
+        fetch(`/geometrydashdotcom/assets/music/-1.mp3`)
+      );
+      return;
+    }
   }
 });
