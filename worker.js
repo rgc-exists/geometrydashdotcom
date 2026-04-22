0b087d06d0b83250d0ab724ae0c38c29c698cb10
RGC Exists <103580754+rgc-exists@users.noreply.github.com>
2026-04-21 21:40:31 -0400

    stuff 2

--- worker.js
+++ worker.js
@@ -13,15 +13,15 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
   if (url.pathname.includes("1.txt")) {
-    event.respondWith(Promise.resolve(levelString));
+    event.respondWith(new Response(levelString), {
+      headers: { "Content-Type": "text/plain" },
+    });
     return;
   }
 
   if (url.pathname.includes("StereoMadness.mp3")) {
     event.respondWith(
-      event.respondWith(
-        fetch(`https://www.newgrounds.com/audio/download/${songID}`),
-      ),
+      fetch(`https://www.newgrounds.com/audio/download/${songID}`),
     );
     return;
   }
